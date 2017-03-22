114e1dffc43fbf0888224187f150bd48095164b0
markdegroat <mhdegroat@gmail.com>
2017-03-22 11:40:31 -0400

    Final

--- lib/student.rb
+++ lib/student.rb
@@ -3,17 +3,100 @@ class Student
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
+    #row is an array of the different column values of a single row
+    new_student = Student.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
+
   end
 
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
+    sql = <<-SQL
+          SELECT *
+          FROM students
+          SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
 
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+    sql = <<-SQL
+        SELECT *
+        FROM students
+        where name = ?
+        LIMIT 1
+        SQL
+    DB[:conn].execute(sql, name).map do |row|#if we weren't limiting one in the sql query, then we would need to only grab the end.first value
+      self.new_from_db(row)
+    end.first #this end.first means end and return the FIRST value
   end
+
+  def self.count_all_students_in_grade_9
+    sql = <<-SQL
+        SELECT *
+        FROM students
+        where grade = 9
+        SQL
+    DB[:conn].execute(sql).map do |row|#if we weren't limiting one in the sql query, then we would need to only grab the end.first value
+      self.new_from_db(row)
+    end
+  end
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+        SELECT *
+        FROM students
+        WHERE grade<12
+        SQL
+    DB[:conn].execute(sql).map do |row|#if we weren't limiting one in the sql query, then we would need to only grab the end.first value
+      self.new_from_db(row)
+    end
+  end
+
+  def self.first_x_students_in_grade_10(x)
+    sql = <<-SQL
+        SELECT *
+        FROM students
+        WHERE grade = 10
+        LIMIT ?
+        SQL
+    DB[:conn].execute(sql, x).map do |row|#if we weren't limiting one in the sql query, then we would need to only grab the end.first value
+      self.new_from_db(row)
+    end
+  end
+
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+        SELECT *
+        FROM students
+        WHERE grade = 10
+        LIMIT 1
+        SQL
+    DB[:conn].execute(sql).map do |row|#if we weren't limiting one in the sql query, then we would need to only grab the end.first value
+      self.new_from_db(row)
+    end.first
+  end
+
+  def self.all_students_in_grade_x(grade)
+    sql = <<-SQL
+        SELECT *
+        FROM students
+        WHERE grade = ?
+        SQL
+    DB[:conn].execute(sql, grade).map do |row|#if we weren't limiting one in the sql query, then we would need to only grab the end.first value
+      self.new_from_db(row)
+    end
+  end
+
+
   
   def save
     sql = <<-SQL
